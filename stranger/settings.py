de3fb3ecb8fcf2fca1534e5fa485a0c0fd2f38b2
berrybab6 <bedatuassefa@gmail.com>
2021-10-03 10:20:01 +0300

    setting updated

--- stranger/settings.py
+++ stranger/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api.apps.ApiConfig',
     'graphene_django',
 ]
 
@@ -124,3 +125,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+GRAPHENE = {   
+    "SCHEMA": "api.schema.schema"
+}
